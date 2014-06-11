a43e576c692fe8e45a531ea5b0e53a023f86c477
Larry Weya <larryweya@gmail.com>
2014-06-11 14:05:14 +0300

    LW: return None if density is None

--- composting/models/compost_sales_register.py
+++ composting/models/compost_sales_register.py
@@ -1,4 +1,10 @@
 from zope.interface import implementer
+from sqlalchemy import desc
+
+from composting.libs.utils import get_month_start_end
+from composting.models.base import DBSession
+from composting.models.compost_density_register import CompostDensityRegister
+from composting.models.municipality_submission import MunicipalitySubmission
 from composting.models.submission import Submission, ISubmission
 
 
@@ -18,11 +24,47 @@ class CompostSalesRegister(Submission):
 
     LIST_ACTION_NAME = 'outgoing-compost-sales-register'
 
+    _compost_density = None
+    _density = None
+
     @property
     def volume(self):
         if self.json_data[self.IS_BAGGED_COMPOST_FIELD] == 'yes':
             return None
 
         return float(self.json_data[self.COMPOST_LENGTH_FIELD])\
-               * float(self.json_data[self.COMPOST_WIDTH_FIELD])\
-               * float(self.json_data[self.COMPOST_HEIGHT_FIELD])
+            * float(self.json_data[self.COMPOST_WIDTH_FIELD])\
+            * float(self.json_data[self.COMPOST_HEIGHT_FIELD])
+
+    def get_compost_density(self, municipality):
+        start, end = get_month_start_end(self.date)
+        # with our date as ref. find the density for the month
+        self._compost_density = self._compost_density\
+            or DBSession.query(CompostDensityRegister)\
+            .select_from(MunicipalitySubmission)\
+            .join(MunicipalitySubmission.submission)\
+            .filter(MunicipalitySubmission.municipality == municipality,
+                    CompostDensityRegister.date >= start,
+                    CompostDensityRegister.date <= end)\
+            .order_by(desc(CompostDensityRegister.date))\
+            .limit(1)\
+            .first()
+        return self._compost_density
+
+    def density(self, municipality):
+        compost_density = self.get_compost_density(municipality)
+        if compost_density is None:
+            return None
+
+        self._density = self._density or compost_density.density(municipality)
+        return self._density
+
+    def weight(self, municipality):
+        volume = self.volume
+        density = self.density(municipality)
+        if volume is None or density is None:
+            return None
+
+        return (density * volume)/1000
+
+
--- composting/tests/models/test_compost_sales_register.py
+++ composting/tests/models/test_compost_sales_register.py
@@ -1,4 +1,8 @@
+import datetime
+
+from composting.models.municipality import Municipality
 from composting.models.compost_sales_register import CompostSalesRegister
+from composting.models.compost_density_register import CompostDensityRegister
 from composting.tests.test_base import TestBase
 
 
@@ -19,3 +23,70 @@ class TestCompostSalesRegister(TestBase):
                 'compost_height': '5.0'
             })
         self.assertEqual(compost_sale.volume, 60.0)
+
+    def test_get_compost_density(self):
+        self.setup_test_data()
+        municipality = Municipality.get(Municipality.name == "Mukono")
+        compost_sale = CompostSalesRegister(
+            date=datetime.datetime(2014, 05, 01),
+            json_data={
+                'bagged_compost': 'no',
+                'compost_length': '3.0',
+                'compost_width': '4.0',
+                'compost_height': '5.0'
+            })
+        compost_density = compost_sale.get_compost_density(municipality)
+        self.assertIsInstance(compost_density, CompostDensityRegister)
+
+    def test_get_compost_density_returns_none_if_no_density_record(self):
+        self.setup_test_data()
+        municipality = Municipality.get(Municipality.name == "Mukono")
+        compost_sale = CompostSalesRegister(
+            date=datetime.datetime(2013, 01, 01),
+            json_data={
+                'bagged_compost': 'no',
+                'compost_length': '3.0',
+                'compost_width': '4.0',
+                'compost_height': '5.0'
+            })
+        compost_density = compost_sale.get_compost_density(municipality)
+        self.assertIsNone(compost_density)
+
+    def test_weight_calculation(self):
+        self.setup_test_data()
+        municipality = Municipality.get(Municipality.name == "Mukono")
+        compost_sale = CompostSalesRegister(
+            date=datetime.datetime(2014, 05, 01),
+            json_data={
+                'bagged_compost': 'no',
+                'compost_length': '3.0',
+                'compost_width': '4.0',
+                'compost_height': '5.0'
+            })
+        weight = compost_sale.weight(municipality)
+        self.assertAlmostEqual(weight, 864.0/1000)  # div to convert to tonnes
+
+    def test_weight_calculation_returns_none_if_bagged(self):
+        self.setup_test_data()
+        municipality = Municipality.get(Municipality.name == "Mukono")
+        compost_sale = CompostSalesRegister(
+            date=datetime.datetime(2013, 01, 01),
+            json_data={
+                'bagged_compost': 'yes',
+            })
+        weight = compost_sale.weight(municipality)
+        self.assertIsNone(weight)
+
+    def test_weight_calculation_returns_none_if_density_is_none(self):
+        self.setup_test_data()
+        municipality = Municipality.get(Municipality.name == "Mukono")
+        compost_sale = CompostSalesRegister(
+            date=datetime.datetime(2013, 01, 01),
+            json_data={
+                'bagged_compost': 'no',
+                'compost_length': '3.0',
+                'compost_width': '4.0',
+                'compost_height': '5.0'
+            })
+        weight = compost_sale.weight(municipality)
+        self.assertIsNone(weight)
