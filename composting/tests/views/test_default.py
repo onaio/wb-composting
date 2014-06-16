4b5b266ff66acd8b8613164dbcaf3ee684a489fe
Larry Weya <larryweya@gmail.com>
2014-06-16 15:01:40 +0300

    redirect to municipalities list rom default url

--- composting/tests/views/test_default.py
+++ composting/tests/views/test_default.py
@@ -6,15 +6,11 @@ from composting.tests.test_base import (
 from composting.views.default import default
 
 
-class TestDefault(IntegrationTestBase):
-    def test_home_view_response(self):
-        request = testing.DummyRequest()
-        response = default(request)
-        self.assertEqual(response, {})
-
-
 class TestDefaultFunctional(FunctionalTestBase):
     def test_home_view_response(self):
         url = self.request.route_url('default')
         response = self.testapp.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.location,
+            self.request.route_url('municipalities', traverse=()))
--- composting/views/default.py
+++ composting/views/default.py
@@ -1,8 +1,9 @@
 from pyramid.view import (
     view_config,
 )
+from pyramid.httpexceptions import HTTPFound
 
 
 @view_config(route_name='default', renderer='default.jinja2')
 def default(request):
-    return {}
+    return HTTPFound(request.route_url('municipalities', traverse=()))
