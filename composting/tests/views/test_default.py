aec205b59445a041ee8a6a2088035b5b538cf0ab
Larry Weya <larryweya@gmail.com>
2014-06-18 13:27:52 +0300

    LW: factor in auth in default view test

--- composting/tests/views/test_default.py
+++ composting/tests/views/test_default.py
@@ -8,9 +8,12 @@ from composting.views.default import default
 
 class TestDefaultFunctional(FunctionalTestBase):
     def test_home_view_response(self):
+        self.setup_test_data()
         url = self.request.route_url('default')
-        response = self.testapp.get(url)
+        headers = self._login_user(1)
+        response = self.testapp.get(url, headers=headers)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
             response.location,
             self.request.route_url('municipalities', traverse=()))
+        response.follow(headers=headers)
