bed9f790d1e4bcd93dba791924d6889d2fce6968
geoffreymuchai <geoffreymuchai@gmail.com>
2014-08-07 13:30:46 +0300

    GM: Added development database initialisation script

--- composting/scripts/initializedb.py
+++ composting/scripts/initializedb.py
@@ -1,18 +1,27 @@
 import os
 import sys
 import transaction
+import json
 
 from sqlalchemy import engine_from_config
 
 from pyramid.paster import (
     get_appsettings,
-    setup_logging,
-    )
+    setup_logging,)
 
 from ..models.base import (
     DBSession,
-    Base,
-    )
+    Base,)
+
+from dashboard.libs.submission_handler import (
+    submission_handler_manager)
+from composting.models import (
+    Municipality,
+    Skip)
+from composting import hook_submission_handlers
+from composting.models.user import User
+from composting.tests.test_base import TestBase
+from composting.security import pwd_context
 
 
 def usage(argv):
@@ -27,7 +36,37 @@ def main(argv=sys.argv):
         usage(argv)
     config_uri = argv[1]
     setup_logging(config_uri)
+
     settings = get_appsettings(config_uri)
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
+
     Base.metadata.create_all(engine)
+
+    setup_logging(config_uri)
+
+    pwd_context.load_path(config_uri)
+
+    # create models here
+    admin = User(username='admin', password='admin', active=True,
+                 group='nema')
+    municipality = Municipality(name="Mukono")
+    manager = User(username='manager', password='manager',
+                   active=True, group='sm', municipality=municipality)
+    skip_a = Skip(municipality=municipality, skip_type='A', small_length=20,
+                  large_length=30, small_breadth=10, large_breadth=16)
+
+    submission_handler_manager.clear()
+    hook_submission_handlers()
+
+    with transaction.manager:
+        admin.save()
+        municipality.save()
+        manager.save()
+        skip_a.save()
+
+        for status, raw_json in TestBase.submissions:
+            json_payload = json.loads(raw_json)
+            handler_class = submission_handler_manager.find_handler(
+                json_payload)
+            handler_class().__call__(json_payload, status=status)
